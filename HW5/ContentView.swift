c49cbbdc38cde3792f39de05a1f6974d404ffa3b
vito4488778-arch <vito.4488778@gmail.com>
2025-11-10 01:04:27 +0800

    Initial Commit

---
+++ HW5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW5
+//
+//  Created by 114iosClassStudent04 on 2025/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW5/HW5App.swift
@@ -0,0 +1,17 @@
+//
+//  HW5App.swift
+//  HW5
+//
+//  Created by 114iosClassStudent04 on 2025/11/10.
+//
+
+import SwiftUI
+
+@main
+struct HW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
